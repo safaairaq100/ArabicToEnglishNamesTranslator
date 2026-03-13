646b07af0e69f117895183c9e752066ae00719d0
Safaa Abbas <safaairaq100@gmail.com>
2026-03-13 15:36:05 +0300

    Add project files.

---
+++ ArabicToEnglishNamesTranslator/NamesTranslatorService.cs
@@ -0,0 +1,5 @@
+﻿namespace ArabicToEnglishNamesTranslator;
+
+public class NamesTranslatorService: INamesTranslatorService
+{
+}
---
+++ ArabicToEnglishNamesTranslator/NamesTranslatorServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ArabicToEnglishNamesTranslator
+{
+    public static class NamesTranslatorServiceExtensions
+    {
+
+        public static IServiceCollection AddArabicCurrencyService(this IServiceCollection services)
+        {
+            services.AddSingleton<INamesTranslatorService, NamesTranslatorService>();
+            return services;
+        }
+        public static INamesTranslatorService UseArabicTextCurrency(this IServiceProvider provider)
+            => provider.GetRequiredService<INamesTranslatorService>();
+
+    }
+}
